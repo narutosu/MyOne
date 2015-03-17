1a0dae01c2995b233ed88ba1e9d796a50a811ebd
sugen <1207173433@qq.com>
2015-03-17 22:22:50 +0800

    no change

--- Classes/AppDelegate.cpp
+++ Classes/AppDelegate.cpp
@@ -1,5 +1,4 @@
-#include "AppDelegate.h"
-#include "HelloWorldScene.h"
+﻿#include "AppDelegate.h"
 
 USING_NS_CC;
 
@@ -27,10 +26,6 @@ bool AppDelegate::applicationDidFinishLaunching() {
     director->setAnimationInterval(1.0 / 60);
 
     // create a scene. it's an autorelease object
-    auto scene = HelloWorld::createScene();
-
-    // run
-    director->runWithScene(scene);
 
     return true;
 }
